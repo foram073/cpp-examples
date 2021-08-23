0b57db4b67cf171e18dc93c1be9a9008b811e495
foram073 <88722877+foram073@users.noreply.github.com>
2021-08-23 14:37:37 +0530

    Update ex2.cpp

--- Lesson-2/ex2.cpp
+++ Lesson-2/ex2.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+double quotient(int a, int b)
+{
+	double res = a / b;
+	return res;
+}
+
+int main()
+{
+	double ans = quotient(5,3);
+	cout <<"Answer is " <<ans <<endl;
+	return 0;
+}
