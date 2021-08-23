2c5491290922de14fe44e0b933702f9031ec3e58
foram073 <88722877+foram073@users.noreply.github.com>
2021-08-23 14:38:32 +0530

    Update ex3.cpp

--- Lesson-2/ex3.cpp
+++ Lesson-2/ex3.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	char op;
+	int num1,num2;
+	
+	cout <<"Enter first number ";
+	cin >> num1;
+	cout <<"Enter second number ";
+	cin >> num2;
+	cout <<"Enter anyone operator from list - +, -, *, / ---- ";
+	cin >> op;
+	
+	switch(op)
+	{
+		case '+':
+		cout <<num1 + num2 <<endl;
+		break;
+		
+		case '-':
+		cout <<num1 - num2 <<endl;
+		break;
+		
+		case '*':
+		cout <<num1 * num2 <<endl;
+		break;
+		
+		case '/':
+		cout <<num1 / num2 <<endl;
+		break;
+		
+		default:
+		cout <<"Invalid operator" <<endl;
+		break;
+	}
+	return 0;
+}
