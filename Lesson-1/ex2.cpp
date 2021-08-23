7fd4eaeaf6d3e425045c54dd1ececc7c2699f359
foram073 <88722877+foram073@users.noreply.github.com>
2021-08-23 14:31:10 +0530

    Update ex2.cpp

--- Lesson-1/ex2.cpp
+++ Lesson-1/ex2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	char a = 'A';
+	bool b = true;
+	double c = 90.09;
+	
+	cout <<"char value " <<a <<endl;
+	cout <<std::boolalpha <<"bool value " <<b <<endl;
+	cout <<"double value " <<c <<endl;
+	return 0;
+}
