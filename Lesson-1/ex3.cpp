f4e6cb467dcfc7f4e121115cc21b8e5e93d3382e
foram073 <88722877+foram073@users.noreply.github.com>
2021-08-23 14:34:41 +0530

    Update ex3.cpp

--- Lesson-1/ex3.cpp
+++ Lesson-1/ex3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	int sum, mul;
+	cout <<"Enter first number ";
+	cin >> num1;
+	cout <<"Enter second number ";
+	cin >> num2;
+	sum = num1 + num2;
+	cout << "sum is "<<sum <<endl;
+	mul = num1 * num2;
+	cout <<"multiplication is "<<mul <<endl;
+	return 0;
+}
