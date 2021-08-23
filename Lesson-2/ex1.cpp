a3214d29ddae6f7eef9e1727671b07ec61ef7238
foram073 <88722877+foram073@users.noreply.github.com>
2021-08-23 14:36:54 +0530

    Update ex1.cpp

--- Lesson-2/ex1.cpp
+++ Lesson-2/ex1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+void product(int add, int minus)
+{
+	cout <<"Addition is " <<add <<endl;
+	cout <<"subtraction is "<<minus <<endl;
+}
+
+int main()
+{
+	int num1 = 10, num2 = 5;
+	int plus = num1 + num2;
+	int sub = num1 - num2;
+	product(plus,sub);
+	return 0;
+}
